1df8272cd8ca0bfce76b5572b74791f01e529005
rupesh <rpiwal02@gmail.com>
2026-01-10 20:20:55 +0530

    fix: resolve OAuth login failure due to missing account_id column

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,20 +1,20 @@
 import { db } from "@/app/db";
+import { schema } from "@/app/db/schema";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-
-import * as schema from "../app/db/schema";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
-    schema,
+    schema: schema,
   }),
-
-  baseURL: process.env.BETTER_AUTH_URL,
   socialProviders: {
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     },
   },
+  plugins: [nextCookies()],
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
